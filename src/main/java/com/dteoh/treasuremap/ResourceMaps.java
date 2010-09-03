56f339c4e435c3eea2c886a5d293982e0fcc820a
dteoh <dteoh@tidal-app.org>
2010-09-03 21:28:47 +1000

    Added additional API methods for creating ResourceMaps comprised of multiple class bundles.

--- src/main/java/com/dteoh/treasuremap/ResourceMaps.java
+++ src/main/java/com/dteoh/treasuremap/ResourceMaps.java
@@ -3,6 +3,9 @@
  */
 package com.dteoh.treasuremap;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.jdesktop.application.ResourceMap;
 
 /**
@@ -28,6 +31,10 @@ public final class ResourceMaps {
      * @return The newly created ResourceMap.
      */
     public static ResourceMap create(final Class<?> c) {
+        if (c == null) {
+            throw new NullPointerException("Class cannot be null.");
+        }
+
         ResourceMap rmap = new ResourceMap(null, c.getClassLoader(), c
                 .getPackage().getName() + ".resources." + c.getSimpleName());
         return rmap;
@@ -50,9 +57,55 @@ public final class ResourceMaps {
      * @return The newly created ResourceMap.
      */
     public static ResourceMap create(final ResourceMap parent, final Class<?> c) {
+        if (c == null) {
+            throw new NullPointerException("Class cannot be null.");
+        }
+
         ResourceMap rmap = new ResourceMap(parent, c.getClassLoader(), c
                 .getPackage().getName() + ".resources." + c.getSimpleName());
         return rmap;
     }
 
+    /**
+     * Create a ResourceMap that includes all resources for the given classes.
+     * 
+     * @param c
+     *            Classes to create the ResourceMap for.
+     * @return The newly created ResourceMap.
+     */
+    public static ResourceMap createMulti(final Class<?>... c) {
+        return createMulti(null, c);
+    }
+
+    /**
+     * Create a ResourceMap that includes all resources for the given classes as
+     * well as the parent resource map.
+     * 
+     * @param parent
+     *            Parent resource map.
+     * @param c
+     *            Classes to create the ResourceMap for.
+     * @return The newly created ResourceMap.
+     */
+    public static ResourceMap createMulti(final ResourceMap parent,
+            final Class<?>... c) {
+        if (c == null || c.length == 0) {
+            throw new NullPointerException("Classes cannot be null.");
+        }
+
+        ClassLoader loader = null;
+
+        List<String> bundleNames = new LinkedList<String>();
+        for (Class<?> bundle : c) {
+            if (c == null) {
+                continue;
+            }
+            loader = bundle.getClassLoader();
+            bundleNames.add(bundle.getPackage().getName() + ".resources."
+                    + bundle.getSimpleName());
+        }
+
+        ResourceMap rmap = new ResourceMap(parent, loader, bundleNames);
+        return rmap;
+    }
 }
